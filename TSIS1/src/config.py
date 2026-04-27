155c6168227ea0e25e216019fa8ba14c3b963186
izzzzyyy01 <i_orazbayeva@kbtu.kz>
2026-04-27 14:16:40 +0500

    Update config.py

--- TSIS1/src/config.py
+++ TSIS1/src/config.py
@@ -1,1 +1,7 @@
-
+# Данные для подключения к PostgreSQL
+params = {
+    "host": "localhost",
+    "database": "phonebook",
+    "user": "postgres",
+    "password": "your_password" # Тут твой пароль от базы
+}
