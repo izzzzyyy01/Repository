3169dc7008d6fbaa2aae827db07c18a4eabc1367
izzzzyyy01 <i_orazbayeva@kbtu.kz>
2026-04-27 14:28:52 +0500

    Update constants.py

--- TSIS2/src/constants.py
+++ TSIS2/src/constants.py
@@ -1,15 +1,17 @@
 import pygame
 
-# Цвета
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
-BLUE = (0, 0, 255)
 GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
 
 # Размеры кистей (Задача 3.2)
 BRUSH_SIZES = {
-    "1": 2,
-    "2": 5,
-    "3": 10
+    pygame.K_1: 2,
+    pygame.K_2: 5,
+    pygame.K_3: 10
 }
+
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
