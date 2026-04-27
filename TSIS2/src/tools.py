2b4269a5b766fc22b1b11a47e48ce79cc4ee52c9
izzzzyyy01 <i_orazbayeva@kbtu.kz>
2026-04-27 14:27:44 +0500

    Update tools.py

--- TSIS2/src/tools.py
+++ TSIS2/src/tools.py
@@ -1,17 +1,22 @@
 import pygame
 
-def flood_fill(surface, x, y, fill_color):
+def flood_fill(surface, x, y, new_color):
     target_color = surface.get_at((x, y))
-    if target_color == fill_color:
+    if target_color == new_color:
         return
     
-    pixels_to_fill = [(x, y)]
-    while pixels_to_fill:
-        cx, cy = pixels_to_fill.pop()
+    stack = [(x, y)]
+    while stack:
+        cx, cy = stack.pop()
         if surface.get_at((cx, cy)) == target_color:
-            surface.set_at((cx, cy), fill_color)
-            # Проверяем соседей (вправо, влево, вверх, вниз)
-            if cx + 1 < surface.get_width(): pixels_to_fill.append((cx + 1, cy))
-            if cx - 1 >= 0: pixels_to_fill.append((cx - 1, cy))
-            if cy + 1 < surface.get_height(): pixels_to_fill.append((cx, cy + 1))
-            if cy - 1 >= 0: pixels_to_fill.append((cx, cy - 1))
+            surface.set_at((cx, cy), new_color)
+            if cx + 1 < surface.get_width(): stack.append((cx + 1, cy))
+            if cx - 1 >= 0: stack.append((cx - 1, cy))
+            if cy + 1 < surface.get_height(): stack.append((cx, cy + 1))
+            if cy - 1 >= 0: stack.append((cx, cy - 1))
+
+
+def draw_text(surface, text, pos, color, font_size=30):
+    font = pygame.font.SysFont("Arial", font_size)
+    text_surf = font.render(text, True, color)
+    surface.blit(text_surf, pos)
